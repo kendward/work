753a3d4eb5304ef3051a55e6e18a76eea1d17fa8
Zeeshan Ali <mesubhan12@gmail.com>
2024-08-18 19:28:31 +0500

    update config

--- server/src/modules/common/swagger/constants.ts
+++ server/src/modules/common/swagger/constants.ts
@@ -1,5 +1,10 @@
+import { APP_ENV } from 'src/modules/utils/constants';
+
 export const SWAGGER_API_ROOT = 'api/docs';
 export const SWAGGER_API_NAME = 'KLAYD v1 API';
 export const SWAGGER_API_DESCRIPTION = 'API Documentation for KLAYD v1';
 export const SWAGGER_API_CURRENT_VERSION = '1.0';
-export const SERVER_URL = `${process.env.APP_URL}:${process.env.APP_PORT}`;
+export const SERVER_URL =
+  process.env.NODE_ENV === APP_ENV.DEVELOPMENT
+    ? `${process.env.APP_URL}:${process.env.APP_PORT}`
+    : process.env.APP_URL;
